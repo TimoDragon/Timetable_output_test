ccab9d2183fd817f828a07525c40c6e13f0fa033
TimoDragon <timo.kruse@hebk.de>
2022-06-02 23:02:11 +0200

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        Timetable table = new Timetable();
+        table.printTimetable();
+    }
+}
---
+++ src/Timetable.java
@@ -0,0 +1,85 @@
+public class Timetable {
+    String[][] table = new String[5][10];
+
+    public Timetable() {
+        table[0][0] = "Montag";
+        table[0][1] = "Informatik";
+        table[0][2] = "Mathe";
+        table[0][3] = "Deutsch";
+
+        table[1][0] = "Dienstag";
+        table[1][1] = "Englisch";
+        table[1][2] = "GLG";
+        table[1][3] = "BWL";
+
+        table[2][0] = "Mittwoch";
+        table[2][1] = "Informatik";
+        table[2][2] = "Sport";
+        table[2][3] = "Sport";
+        table[2][4] = "Spanisch";
+
+        table[3][0] = "Donnerstag";
+        table[3][1] = "Spanisch";
+        table[3][2] = "BWL";
+
+        table[4][0] = "Freitag";
+        table[4][1] = "Psychologie";
+        table[4][2] = "Philosophie";
+    }
+    
+    public void printTimetable() {
+        int[] maxLengths = { 0, 0, 0, 0, 0 };
+        int maxHeight = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (table[i][j] != null) {
+                    if (table[i][j].length() > maxLengths[i]) {
+                        maxLengths[i] = table[i][j].length();
+                    }
+
+                    if (j > maxHeight) {
+                        maxHeight = j;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < maxHeight + 1; i++) {
+            printline(maxLengths);
+
+            for (int j = 0; j < 5; j++) {
+                if (table[j][i] != null) {
+                    System.out.print("| " + table[j][i] + " ");
+                }
+                else {
+                    System.out.print("|  ");
+                }
+
+                if (table[j][i] != null) {
+                    for (int k = 0; k < maxLengths[j] - table[j][i].length(); k++) {
+                        System.out.print(" ");
+                    }
+                }
+                else {
+                    for (int k = 0; k < maxLengths[j]; k++) {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println("|");
+        }
+
+        printline(maxLengths);
+    }
+
+    void printline(int[] maxLengths) {
+        for (int j = 0; j < 5; j++) {
+            System.out.print("+");
+            for (int k = 0; k < maxLengths[j] + 2; k++) {
+                System.out.print("-");
+            }
+        }
+        System.out.println("+");
+    }
+}
